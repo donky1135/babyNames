c31c4f3bbd22d20a344363cfb6fa26c066ecccc9
Alex <omega.alpha.psi66@gmail.com>
2019-05-30 23:15:39 -0400

    Figuring out how to extract data from website

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 import org.jsoup.*;
 import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args)throws IOException {
         Document doc = Jsoup.connect("https://www.ssa.gov/oact/babynames/decades/names1960s.html").get();
+        Elements Data = doc.select("tbody").select("td");
+//        System.out.println(Data);
+        for(Element i : Data)
+            System.out.println(i.text());
 
     }
 }
