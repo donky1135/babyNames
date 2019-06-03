81a267ff90cd955ddb45395c5e63fe6fc4577f0c
Valentino <21AV0740@westwindsor.k12.nj.us>
2019-06-03 08:40:37 -0400

    Need to figure out how to get the numberings & info about db out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.jsoup.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
@@ -9,19 +14,26 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args)throws IOException {
+        File file = new File("C:\\Users\\21av0740\\Desktop\\topBabyNames60s.json");
         Document doc = Jsoup.connect("https://www.ssa.gov/oact/babynames/decades/names1960s.html").get();
         Elements Data = doc.select("tbody").select("td");
         TreeMap<Integer, String> dataB4 = new TreeMap<Integer, String>();
-        Pattern pop = Pattern.compile("/d/d/d");
+        Pattern pop = Pattern.compile("/d/d/d{3,}");
         Pattern namin = Pattern.compile("[a-zA-Z]");
         int temp =0;
         String name="";
+        TreeMap<Integer,String> map = new TreeMap<Integer, String>();
+
+        ObjectMapper mapin = new ObjectMapper();
+
+        ObjectWriter writin = mapin.writer(new DefaultPrettyPrinter());
+
         for(Element i : Data){
             Matcher r = pop.matcher(i.text());
             Matcher p = namin.matcher(i.text());
             if(r.find( ))
                 continue;
-            else if ((i.text().length() > 2) && !p.find()){
+            else if ((i.text().length() > 3) && !p.find()){
                 temp = Integer.parseInt(i.text().replace(",", ""));
             }
             else if (p.find())
@@ -29,7 +41,7 @@ public class Main {
             dataB4.put(temp, name);
         }
         System.out.println(dataB4);
-
-
+        file.delete();
+        writin.writeValue(file, dataB4);
     }
 }
